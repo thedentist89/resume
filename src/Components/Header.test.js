5fe355c8d3ab51a4f80dfbb2e089276fce78d8f7
Dermoumi S <sdermoumi@gmail.com>
2019-01-19 15:03:17 +0100

    Fix file path

--- src/Components/Header.test.js
+++ src/Components/Header.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { mount, shallow } from 'enzyme'
 
 import Header from './Header'
-import data from '../data/header'
+import data from '../Data/header'
 
 describe('Header component', () => {
   let wrapper
