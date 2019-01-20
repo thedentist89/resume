eb751b48569fab171256bdda0054b371dad72e2f
Dermoumi S <sdermoumi@gmail.com>
2019-01-20 23:08:48 +0100

    Added a dark theme

--- src/style.js
+++ src/style.js
@@ -6,5 +6,17 @@ export default {
     primary: 'hsl(222, 22%, 15%)',
     secondary: 'hsl(210, 8%, 45%)',
     accent: 'hsl(209, 89%, 44%)',
+
+    lineart: 'hsl(205, 7%, 33%)',
+  },
+  dark: {
+    fg: 'hsl(70, 6%, 79%)',
+    bg: 'hsl(216, 33%, 6%)',
+
+    primary: 'hsl(0, 0%, 98%)',
+    secondary: 'hsl(219, 10%, 50%)',
+    accent: 'hsl(28, 100%, 63%)',
+
+    lineart: 'hsl(219, 10%, 50%)',
   },
 }
